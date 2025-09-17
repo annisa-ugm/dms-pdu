883c3eb64051f29fb71faaebd5b54e4e4762683d
annisa-ugm <annisar496@gmail.com>
2025-09-17 23:00:48 +0700

    chore(migration): update files table schema definition

--- database/migrations/2025_09_12_171135_create_files_table.php
+++ database/migrations/2025_09_12_171135_create_files_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('files', function (Blueprint $table) {
-            $table->string('id', 255)->primary();
+            $table->id()->primary();
             $table->string('name', 1024);
             $table->string('path', 1024)->nullable();
             $table->string('storage_path', 1024)->nullable();
