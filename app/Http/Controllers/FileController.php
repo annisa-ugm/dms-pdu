ee3f2f94282039654915537c1abd3bae01dca11e
annisa-ugm <annisar496@gmail.com>
2025-09-17 18:06:23 +0700

    feat(file): add myFiles endpoint to list user files

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\File;
+use App\Http\Resources\FileResource;
+
+class FileController extends Controller
+{
+    public function myFiles(Request $request, string $folder = null)
+    {
+        $search = $request->get('search');
+
+        if ($folder) {
+            $folder = File::query()
+                ->where('created_by', Auth::id())
+                ->where('path', $folder)
+                ->firstOrFail();
+        }
+        if (!$folder) {
+            $folder = $this->getRoot();
+        }
+
+        $query = File::query()
+            ->select('files.*')
+            // ->with('starred')
+            ->where('created_by', Auth::id())
+            ->where('_lft', '!=', 1)
+            ->orderBy('is_folder', 'desc')
+            ->orderBy('files.created_at', 'desc')
+            ->orderBy('files.id', 'desc');
+
+        if ($search) {
+            $query->where('name', 'like', "%$search%");
+        } else {
+            $query->where('parent_id', $folder->id);
+        }
+
+        $files = $query->paginate(10);
+
+        $files = FileResource::collection($files);
+
+        if ($request->wantsJson()) {
+            return $files;
+        }
+
+        $ancestors = FileResource::collection([...$folder->ancestors, $folder]);
+
+        $folder = new FileResource($folder);
+
+        return response()->json([
+            'files' => $files,
+            'folder' => $folder,
+            'ancestors' => $ancestors,
+        ]);
+
+    }
+
+    public function createFolder(StoreFolderRequest $request)
+    {
+        $data = $request->validated();
+        $parent = $request->parent;
+
+        if (!$parent) {
+            $parent = $this->getRoot();
+        }
+
+        $file = new File();
+        $file->is_folder = 1;
+        $file->name = $data['name'];
+
+        $parent->appendNode($file);
+    }
+
+    public function store(StoreFileRequest $request)
+    {
+        $data = $request->validated();
+        $parent = $request->parent;
+        $user = $request->user();
+        $fileTree = $request->file_tree;
+
+        if (!$parent) {
+            $parent = $this->getRoot();
+        }
+
+        if (!empty($fileTree)) {
+            $this->saveFileTree($fileTree, $parent, $user);
+        } else {
+            foreach ($data['files'] as $file) {
+                /** @var \Illuminate\Http\UploadedFile $file */
+
+                $this->saveFile($file, $user, $parent);
+            }
+        }
+    }
+
+    private function getRoot()
+    {
+        return File::query()->whereIsRoot()->where('created_by', Auth::id())->firstOrFail();
+    }
+
+    public function saveFileTree($fileTree, $parent, $user)
+    {
+        foreach ($fileTree as $name => $file) {
+            if (is_array($file)) {
+                $folder = new File();
+                $folder->is_folder = 1;
+                $folder->name = $name;
+
+                $parent->appendNode($folder);
+                $this->saveFileTree($file, $folder, $user);
+            } else {
+
+                $this->saveFile($file, $user, $parent);
+            }
+        }
+    }
+
+    private function saveFile($file, $user, $parent): void
+    {
+        $path = $file->store('/files/' . $user->id, 'local');
+
+        $model = new File();
+        $model->storage_path = $path;
+        $model->is_folder = false;
+        $model->name = $file->getClientOriginalName();
+        $model->mime = $file->getMimeType();
+        $model->size = $file->getSize();
+        $model->uploaded_on_cloud = 0;
+
+        $parent->appendNode($model);
+
+        UploadFileToCloudJob::dispatch($model);
+    }
+}
