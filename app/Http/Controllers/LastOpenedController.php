83372fcff2e11cf71bc408e5a6cb5697c6f88e9d
annisa-ugm <annisar496@gmail.com>
2025-11-30 19:55:31 +0700

    feat: filtering rule

--- app/Http/Controllers/LastOpenedController.php
+++ app/Http/Controllers/LastOpenedController.php
@@ -8,10 +8,11 @@ use App\Models\FileAccessLog;
 use App\Http\Resources\FileResource;
 use Illuminate\Support\Facades\Auth;
 use App\Traits\SortableFileQuery;
+use App\Traits\FilterableFileQuery;
 
 class LastOpenedController extends Controller
 {
-    use SortableFileQuery;
+    use SortableFileQuery, FilterableFileQuery;
 
     public function lastOpenedFiles(Request $request)
     {
@@ -27,6 +28,8 @@ class LastOpenedController extends Controller
                 ->whereNotNull('files.parent_id')
                 ->select('files.*', 'log.last_accessed_at');
 
+            $query = $this->applyDmsFiltering($query, $request);
+            
             // primary sort: last opened first
             $query->orderBy('log.last_accessed_at', 'desc');
 
